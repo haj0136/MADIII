56f5fece38a16edaa230d9235812ab1c64d87923
Marek Hajdík <hajdik.marek1@gmail.com>
2019-10-04 18:33:45 +0200

    cv3 datasets analyzing

--- cv3/__init__.py
+++ cv3/__init__.py
@@ -5,14 +5,49 @@ import sklearn.metrics
 import scipy.spatial
 import pandas as pd
 
-if __name__ == '__main__':
-    data = np.loadtxt("densegrid.csv", delimiter=";")
-    distance_matrix = scipy.spatial.distance_matrix(data, data)
+
+def plot_closest_distance(matrix):
+    distance_matrix = scipy.spatial.distance_matrix(matrix, matrix)
     closest_neighbor_distance = np.where(distance_matrix == 0, 99, distance_matrix).min(axis=0)
     pd.Series(closest_neighbor_distance).hist(bins=40)
     plt.show()
 
 
+def plot_db_scan(matrix, eps: float, min_samples: int):
+    clusters_ = sklearn.cluster.DBSCAN(eps=eps, min_samples=min_samples).fit(matrix)
+    plt.scatter(matrix[:, 0], matrix[:, 1], c=clusters_.labels_)
+    plt.show()
+    return clusters_
+
+
+def plot_kmeans(matrix, no_clusters: int):
+    clusters_ = sklearn.cluster.KMeans(n_clusters=no_clusters).fit(matrix)
+    plt.scatter(matrix[:, 0], matrix[:, 1], c=clusters_.labels_)
+    plt.show()
+    return clusters_
+
+
+def print_clusters_size(clusters_):
+    db_labels = clusters_.labels_
+    labels, counts = np.unique(db_labels[db_labels >= 0], return_counts=True)
+    noisy_labels, noisy_counts = np.unique(db_labels[db_labels < 0], return_counts=True)
+
+    for i in range(len(labels)):
+        print("Cluster: {0} size: {1} ".format(labels[i], counts[i]))
+    if noisy_labels.size > 0:
+        print("Noisy cluster size: {0} ".format(noisy_counts[0]))
+    else:
+        print("Noisy cluster size: 0")
+
+
+if __name__ == '__main__':
+    data_densegrid = np.loadtxt("densegrid.csv", delimiter=";")
+    data_boxes = np.loadtxt("boxes.csv", delimiter=";")
+    data_annulus = np.loadtxt("annulus.csv", delimiter=";")
+    plot_closest_distance(data_boxes)
+    plot_closest_distance(data_densegrid)
+
+    '''
     clustering_scores = []
     for k in range(2, 15):
         clusters = sklearn.cluster.KMeans(n_clusters=k).fit(data)
@@ -27,18 +62,25 @@ if __name__ == '__main__':
     plt.show()
     df_score.silhouette.plot()
     plt.show()
-
+    '''
     # Agglomerative
+    '''
     clusters = sklearn.cluster.AgglomerativeClustering(n_clusters=10, affinity="euclidean", linkage="single").fit(data)
     plt.scatter(data[:, 0], data[:, 1], c=clusters.labels_)
     plt.show()
+    '''
     # K-means
-    clusters = sklearn.cluster.KMeans(n_clusters=10).fit(data)
-    plt.scatter(data[:, 0], data[:, 1], c=clusters.labels_)
-    plt.show()
-    # DBSCAN
-    clusters = sklearn.cluster.DBSCAN(eps=0.15, min_samples=10).fit(data)
-    plt.scatter(data[:, 0], data[:, 1], c=clusters.labels_)
-    plt.show()
+    plot_kmeans(data_densegrid, 10)
+
+    # DBSCAN Dense grid
+    clusters = plot_db_scan(data_densegrid, 0.20, 10)
+    # DBSCAN boxes
+    clusters = plot_db_scan(data_boxes, 2.5, 2)
+    print_clusters_size(clusters)
+
+    # DBSCAN annulus
+    clusters = plot_db_scan(data_annulus, 2, 2)
+
+
 
     print()
