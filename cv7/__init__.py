770c3dbdac557377564f53d351b6fdc95d717feb
Marek Hajdík <hajdik.marek1@gmail.com>
2019-11-05 00:05:44 +0100

    2d visualization

--- cv7/__init__.py
+++ cv7/__init__.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def import_data(colnames):
@@ -17,24 +18,67 @@ def import_data(colnames):
     return data
 
 
+def plot_svm(models, x, y):
+    h = .02  # step size in the mesh
+    # create a mesh to plot in
+    x_min, x_max = x.iloc[:, 0].min() - 1, x.iloc[:, 0].max() + 1
+    y_min, y_max = x.iloc[:, 1].min() - 1, x.iloc[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # title for the plots
+    titles = ['SVC with linear kernel C=1',
+              'Linear SVC C = 10',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel']
+
+    for i, clf in enumerate(models):
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        plt.subplot(2, 2, i + 1)
+        plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+
+        # Plot also the training points
+        plt.scatter(x.iloc[:, 0], x.iloc[:, 1], c=y, cmap=plt.cm.coolwarm)
+        plt.xlabel('Sepal length')
+        plt.ylabel('Sepal width')
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xticks(())
+        plt.yticks(())
+        plt.title(titles[i])
+
+    plt.show()
+
+
 if __name__ == '__main__':
     colnames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
     dataset = import_data(colnames)
-    X = dataset.drop(['Class'], axis=1)
+    # X = dataset.drop(['Class'], axis=1)
+    X = dataset.iloc[:, :2]
     Y = dataset['Class']
+    C = 1.0
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
-    svc_poly = SVC(kernel='poly', degree=8)
-    svc_poly.fit(X_train, y_train)
+    svc_linear = SVC(kernel='linear', C=C).fit(X_train, y_train)
+    svc_linear2 = SVC(kernel='linear', C=10).fit(X_train, y_train)
+    svc_poly = SVC(kernel='poly', degree=3).fit(X_train, y_train)
 
-    y_pred = svc_poly.predict(X_test)
-    print("Polynomial")
+    y_pred = svc_linear.predict(X_test)
+    print(f"Linear C = {C}")
     print()
     print(classification_report(y_test, y_pred))
 
     svc_rbf = SVC(kernel='rbf', C=10)
     svc_rbf.fit(X_train, y_train)
     y_pred = svc_rbf.predict(X_test)
-    print("RBF")
+    print(f"RBF C = {C}")
     print()
     print(classification_report(y_test, y_pred))
+    plot_svm([svc_linear, svc_linear2, svc_rbf, svc_poly], X, Y)
